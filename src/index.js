2f0dfe484e9e823f13efda33eaf65b53bb404208
mehedihasan250 <mehedi@orangetoolz.com>
2022-08-30 01:33:36 +0600

    feat: add botstrap and frontend nav

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
