48040f7820cec5d2dd7509b541620718d6bd8b3f
mehedihasan250 <mehedi@orangetoolz.com>
2022-11-13 14:54:01 +0600

    feat: show category list

--- src/service/serviceApi.js
+++ src/service/serviceApi.js
@@ -2,6 +2,10 @@ import { sendRequest } from "./rootApi";
 const GET = 'get';
 const POST = 'post';
 
-export const categoryInsertApi = async (payload) => {
-    return sendRequest(POST, '/api/admin/catagory-store', payload);
+export const categoryInsertApi = async (payload = []) => {
+    return sendRequest(POST, '/api/admin/category-store', payload);
+}
+
+export const categoryListApi = async (payload = []) => {
+    return sendRequest(POST, '/api/admin/category-list', payload);
 }
